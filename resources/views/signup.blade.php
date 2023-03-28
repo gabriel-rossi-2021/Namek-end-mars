a2bb1aaf604f6a58f5bffb19665807f7a70a327c
gabriel-rossi-2021 <81309938+gabriel-rossi-2021@users.noreply.github.com>
2023-03-28 15:22:03 +0200

    Add form pour les informations complémentaires

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -15,6 +15,7 @@
         <p class="subtitle">Vous avez déjà un compte ? <a href="/login"> Connexion</a></p>
         <p class="or"><span>or</span></p>
         <table>
+            <h3>Informations générales</h3>
             <tr>
                 <td>
                     <label for="titre"> <b>Titre</b></label><br>
@@ -54,12 +55,48 @@
 
             <tr>
                 <td>
-                    <label for="psw"><b>Password</b></label><br>
+                    <label for="psw"><b>Mot de passe</b></label><br>
                     <input type="password" placeholder="Votre mot de passe" name="psw" id="input" required>
                 </td>
                 <td>
+                    <label for="confirm-psw"><b>Confirmation</b></label><br>
+                    <input type="password" placeholder="Encore une fois" name="confirm-psw" id="input" required>
+                </td>
+            </tr>
+            <tr>
+                <td>
                     <label for="birth"><b>Date de naissance</b></label><br>
                     <input type="date" placeholder="Date de naissance" name="birth" id="input" required>
+                </td>
+            </tr>
+        </table>
+        <table>
+            <h3>Informations complémentaires</h3>
+            <tr>
+                <td>
+                    <label for="rue"> <b>Rue</b></label><br>
+                    <input type="text" placeholder="Votre rue" name="rue" id="input" required>
+                </td>
+                <td>
+                    <label for="num-rue"> <b>N° de rue</b></label><br>
+                    <input type="number" placeholder="Votre n° de rue" name="num-rue" id="input" required>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="ville"> <b>Ville</b></label><br>
+                    <input type="text" placeholder="Votre ville" name="ville" id="input" required>
+                </td>
+                <td>
+                    <label for="npa"> <b>NPA</b></label><br>
+                    <input type="number" placeholder="Votre NPA" name="npa" id="input" required>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="pays"> <b>Pays</b></label><br>
+                    <input type="text" placeholder="Votre pays" name="pays" id="input" value="Suisse" disabled required>
+                    <p>*Pour l'instant nous livrons que en Suisse</p>
                 </td>
             </tr>
         </table>
