7de70bbe6df9cbeaa7f381cb5841613077914cea
gabriel-rossi-2021 <81309938+gabriel-rossi-2021@users.noreply.github.com>
2023-03-28 15:06:07 +0200

    Suppression de l'affichage si y a pas d'adresse

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -264,75 +264,39 @@
 
                 <div class="row">
                     <!-- Si l'utilisateur n'a pas d'adresse -->
-                    @if(empty($user->address))
-                        <div class="col-md-6">
-                            <div class="card mb-4 mb-md-0">
-                                <div class="card-body">
-                                    <h4 class="mb-4">Informations complémentaires</h4>
-                                    <div class="row">
-                                        <div class="col-sm-3">
-                                            <p class="mb-0">Rue</p>
-                                        </div>
-                                        <div class="col-sm-9">
-                                            <p class="text-muted mb-0">-</p>
-                                        </div>
+                    <div class="col-md-6">
+                        <div class="card mb-4 mb-md-0">
+                            <div class="card-body">
+                                <h4 class="mb-4">Informations complémentaires</h4>
+                                <div class="row">
+                                    <div class="col-sm-3">
+                                        <p class="mb-0">Rue</p>
                                     </div>
-                                    <hr>
-                                    <div class="row">
-                                        <div class="col-sm-3">
-                                            <p class="mb-0">Ville</p>
-                                        </div>
-                                        <div class="col-sm-9">
-                                            <p class="text-muted mb-0">-</p>
-                                        </div>
+                                    <div class="col-sm-9">
+                                        <p class="text-muted mb-0">{{ $user->address->street }}, {{ $user->address->street_number }}</p>
                                     </div>
-                                    <hr>
-                                    <div class="row">
-                                        <div class="col-sm-3">
-                                            <p class="mb-0">Pays</p>
-                                        </div>
-                                        <div class="col-sm-9">
-                                            <p class="text-muted mb-0">-</p>
-                                        </div>
+                                </div>
+                                <hr>
+                                <div class="row">
+                                    <div class="col-sm-3">
+                                        <p class="mb-0">NPA / Ville</p>
+                                    </div>
+                                    <div class="col-sm-9">
+                                        <p class="text-muted mb-0">{{ $user->address->NPA }} {{ $user->address->city }}</p>
+                                    </div>
+                                </div>
+                                <hr>
+                                <div class="row">
+                                    <div class="col-sm-3">
+                                        <p class="mb-0">Pays</p>
+                                    </div>
+                                    <div class="col-sm-9">
+                                        <p class="text-muted mb-0">{{ $user->address->country }}</p>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    @else
-                        <div class="col-md-6">
-                            <div class="card mb-4 mb-md-0">
-                                <div class="card-body">
-                                    <h4 class="mb-4">Informations complémentaires</h4>
-                                    <div class="row">
-                                        <div class="col-sm-3">
-                                            <p class="mb-0">Rue</p>
-                                        </div>
-                                        <div class="col-sm-9">
-                                            <p class="text-muted mb-0">{{ $user->address->street }}, {{ $user->address->street_number }}</p>
-                                        </div>
-                                    </div>
-                                    <hr>
-                                    <div class="row">
-                                        <div class="col-sm-3">
-                                            <p class="mb-0">Ville</p>
-                                        </div>
-                                        <div class="col-sm-9">
-                                            <p class="text-muted mb-0">{{ $user->address->NPA }} {{ $user->address->city }}</p>
-                                        </div>
-                                    </div>
-                                    <hr>
-                                    <div class="row">
-                                        <div class="col-sm-3">
-                                            <p class="mb-0">Pays</p>
-                                        </div>
-                                        <div class="col-sm-9">
-                                            <p class="text-muted mb-0">{{ $user->address->country }}</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    @endif
+                    </div>
                     <div class="col-md-6">
                         <div class="card mb-4 mb-md-0">
                             <div class="card-body">
